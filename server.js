76f3613472611e1550faafa37524b67f30392ce0
Abdullatif Mnyamisi <abdullatifmnyamis@gmail.com>
2025-05-30 00:18:57 -0700

    modifies server.js

--- server.js
+++ server.js
@@ -26,7 +26,11 @@ console.log("📦 Node.js version:", process.version)
 
 
 //express middleware
-app.use(cors());
+// app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true,
+}));
 app.use(express.json({ limit: "10mb" }))
 app.use(express.urlencoded({ extended: true }))
 
